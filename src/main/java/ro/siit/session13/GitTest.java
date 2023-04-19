c2b21a3fffb274a6bbac91103455e5ae82d6534c
fiatfilip <filip.fiat@haufe-lexware.com>
2023-04-19 18:49:55 +0300

    Update GitTest.java

--- src/main/java/ro/siit/session13/GitTest.java
+++ src/main/java/ro/siit/session13/GitTest.java
@@ -2,6 +2,6 @@ package ro.siit.session13;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Version1.0");
+        System.out.println("Version2.0");
     }
 }
